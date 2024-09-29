3f355d4fbe3a31546a7e0074ee4a99496b28af50
dev-rathore <rathoredvd3001@gmail.com>
2024-09-30 02:07:36 +0530

    build fix

--- src/app/(dashboard)/chat/page.tsx
+++ src/app/(dashboard)/chat/page.tsx
@@ -1,106 +1,18 @@
-'use client';
-
-import { useRouter, useSearchParams } from "next/navigation";
-import ChatConversation from "./chat-conversation";
-import ChatSidebar from "./chat-sidebar";
-import { useEffect, useState } from "react";
-import { Chat, ChatType } from "@/lib/types/chat";
-import { useAuthContext } from "@/contexts/auth-context.provider";
-import { getChatsByUserId } from "@/firebase/chat-db-requests";
-import { Button } from "@/components/ui/button";
-import { HOME_ROUTE } from "@/lib/constants/constants";
+import { Suspense } from "react";
+import ChatUI from "./chat"
 
 const ChatPage = () => {
-  const [chats, setChats] = useState<Chat[]>([]);
-  const [filteredChats, setFilteredChats] = useState<Chat[]>([]);
-  const [selectedType, setSelectedType] = useState<ChatType>(ChatType.FOOD_AI);
-  const [isChatsLoading, setIsChatsLoading] = useState<boolean>(true);
-  const router = useRouter();
-  const {
-    user
-  } = useAuthContext();
-  const params = useSearchParams();
-  const id = params.get("id");
-  const type = params.get("type");
-
-  const userId = user?.id;
-
-  const fetchChatsByUserId = async (userId: string) => {
-    const response = await getChatsByUserId(userId);
-
-    setChats(response);
-
-    let chatType = selectedType;
-    if (type) {
-      chatType = type as ChatType;
-      setSelectedType(chatType);
-    }
-
-    const filteredResponse = response.filter((chat) => chat.type === chatType);
-    setFilteredChats(filteredResponse);
-
-    if (filteredResponse.length !== 0 && id) {
-      router.replace(`/chat?id=${id}&type=${chatType}`);
-    } else if (filteredResponse.length !== 0) {
-      router.replace(`/chat?id=${filteredResponse[0].id}&type=${chatType}`);
-    }
-
-    setIsChatsLoading(false);
-  }
-
-  useEffect(() => {
-    const filteredResponse = chats.filter(chat => chat.type === selectedType);
-    setFilteredChats(filteredResponse);
-  }, [selectedType]);
-
-  useEffect(() => {
-    if (userId) {
-      fetchChatsByUserId(userId);
-    }
-  }, [userId]);
-
-  if (isChatsLoading) {
-    return (
-      <div className="w-full flex items-center justify-center h-full">
-        <p>Loading chats...</p>
-      </div>
-    );
-  }
-
-  if (chats.length === 0) {
-    return (
-      <div className="w-full flex flex-col gap-6 items-center justify-center h-full">
-        <p>No chats found</p>
-        <Button
-          variant="app-primary"
-          onClick={() => router.push(HOME_ROUTE)}
-        >
-          Start first chat
-        </Button>
-      </div>
-    );
-  }
-
-  if (!user) {
-    return (
-      <div className="flex items-center justify-center h-full">
-        <p>Please login to view this page</p>
-      </div>
-    );
-  }
-
   return (
-    <>
-      <ChatSidebar
-        filteredChats={filteredChats}
-        selectedType={selectedType}
-        setSelectedType={setSelectedType}
-      />
-      <main className="flex-1 overflow-y-auto ">
-        <ChatConversation />
-      </main>
-    </>
+    <Suspense
+      fallback={
+        <div className="w-full flex items-center justify-center h-screen">
+          <p>Loading chats...</p>
+        </div>
+      }
+    >
+      <ChatUI />
+    </Suspense>
   );
-};
+}
 
 export default ChatPage;
