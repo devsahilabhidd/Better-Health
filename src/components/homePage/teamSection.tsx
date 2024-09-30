fe06873a78a138e504f6bc545d8d1c33ced1a4b8
Danish Deshmukh <120071472+Danish-Deshmukh@users.noreply.github.com>
2024-09-30 22:59:07 +0530

    add text to home page (#8)

--- src/components/homePage/teamSection.tsx
+++ src/components/homePage/teamSection.tsx
@@ -1,4 +1,4 @@
-import { buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from '@/components/ui/button';
 import {
   Card,
   CardContent,
@@ -6,13 +6,14 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import { Facebook, Instagram, Linkedin } from "lucide-react";
+} from '@/components/ui/card';
+import { Facebook, Instagram, Linkedin } from 'lucide-react';
 
 interface TeamProps {
   imageUrl: string;
   name: string;
   position: string;
+  description: string;
   socialNetworks: SociaNetworkslProps[];
 }
 
@@ -23,71 +24,63 @@ interface SociaNetworkslProps {
 
 const teamList: TeamProps[] = [
   {
-    imageUrl: "https://i.pravatar.cc/150?img=35",
-    name: "Devender ",
-    position: "Product Manager",
+    imageUrl: 'https://i.ibb.co/PrHkLzW/dev.jpg',
+    name: 'Devender ',
+    position: 'Product Manager',
+    description:
+      'Devender ensures that our vision aligns with user needs, overseeing the project from concept to completion and bringing strategic insight to the team.',
     socialNetworks: [
       {
-        name: "Linkedin",
-        url: "https://www.linkedin.com/in/leopoldo-miranda/",
-      },
-      {
-        name: "Facebook",
-        url: "https://www.facebook.com/",
-      },
-      {
-        name: "Instagram",
-        url: "https://www.instagram.com/",
+        name: 'Linkedin',
+        url: 'https://www.linkedin.com/in/devender-singh-rathore/',
       },
     ],
   },
   {
-    imageUrl: "https://i.pravatar.cc/150?img=60",
-    name: "Sahil",
-    position: "Tech Lead",
+    imageUrl: 'https://i.ibb.co/BC71sZx/sahil.jpg',
+    name: 'Sahil',
+    position: 'Frontend Developer',
+    description:
+      'Sahil brings the user interface to life, crafting intuitive and engaging designs that improve user experience and accessibility.',
     socialNetworks: [
       {
-        name: "Linkedin",
-        url: "https://www.linkedin.com/in/leopoldo-miranda/",
+        name: 'Linkedin',
+        url: 'https://www.linkedin.com/in/sahil-quraishi-b40287264/',
       },
       {
-        name: "Facebook",
-        url: "https://www.facebook.com/",
-      },
-      {
-        name: "Instagram",
-        url: "https://www.instagram.com/",
+        name: 'Instagram',
+        url: 'https://www.instagram.com/sahilq312/?hl=en',
       },
     ],
   },
   {
-    imageUrl: "https://i.pravatar.cc/150?img=36",
-    name: "Abhinav",
-    position: "Frontend Developer",
+    imageUrl: 'https://i.ibb.co/1T9ffr6/abhinav.jpg',
+    name: 'Abhinav',
+    position: 'Tech Lead',
+    description:
+      'Abhinav leads our development efforts, making crucial technical decisions and guiding the team in building scalable and robust solutions.',
     socialNetworks: [
       {
-        name: "Linkedin",
-        url: "https://www.linkedin.com/in/leopoldo-miranda/",
+        name: 'Linkedin',
+        url: 'https://www.linkedin.com/in/abhinavmalviya58/',
       },
 
       {
-        name: "Instagram",
-        url: "https://www.instagram.com/",
+        name: 'Instagram',
+        url: 'https://www.instagram.com/abhinavmalviya58/?hl=en',
       },
     ],
   },
   {
-    imageUrl: "https://i.pravatar.cc/150?img=17",
-    name: "Danish",
-    position: "Backend Developer",
+    imageUrl: 'https://i.ibb.co/KFGfF7s/photo.jpg',
+    name: 'Danish',
+    position: 'Backend Developer',
+    description:
+      'Maintains backend systems to keep everything running smoothly.',
     socialNetworks: [
       {
-        name: "Linkedin",
-        url: "https://www.linkedin.com/in/leopoldo-miranda/",
-      },
-      {
-        name: "Facebook",
-        url: "https://www.facebook.com/",
+        name: 'Linkedin',
+        url: 'https://www.linkedin.com/in/danish-deshmukh-3255082a8/',
       },
     ],
   },
@@ -96,70 +89,73 @@ const teamList: TeamProps[] = [
 export const Team = () => {
   const socialIcon = (iconName: string) => {
     switch (iconName) {
-      case "Linkedin":
-        return <Linkedin size="20" />;
+      case 'Linkedin':
+        return <Linkedin size='20' />;
 
-      case "Facebook":
-        return <Facebook size="20" />;
+      case 'Facebook':
+        return <Facebook size='20' />;
 
-      case "Instagram":
-        return <Instagram size="20" />;
+      case 'Instagram':
+        return <Instagram size='20' />;
     }
   };
 
   return (
-    <section
-      id="team"
-      className="container py-24 sm:py-32 p-3"
-    >
-      <h2 className="text-3xl md:text-4xl font-bold">
-        <span className=" text-app-primary bg-clip-text">
-          Our Dedicated{" "}
-        </span>
-            Team
+    <section id='team' className='container py-24 sm:py-32 p-3'>
+      <h2 className='text-3xl md:text-4xl font-bold'>
+        <span className=' text-app-primary bg-clip-text'>Our Dedicated </span>
+        Team
       </h2>
 
-      <p className="mt-4 mb-10 text-xl text-muted-foreground">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
-        dolor pariatur sit!
+      <p className='mt-4 mb-10 text-xl text-muted-foreground'>
+        We are a team of innovators and health enthusiasts committed to creating
+        smarter, healthier lifestyles for everyone. With a diverse background in
+        tech and wellness, we strive to deliver personalized solutions that fit
+        your unique needs.
       </p>
 
-      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8 gap-y-10">
+      <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8 gap-y-10'>
         {teamList.map(
-          ({ imageUrl, name, position, socialNetworks }: TeamProps) => (
+          ({
+            imageUrl,
+            name,
+            position,
+            description,
+            socialNetworks,
+          }: TeamProps) => (
             <Card
               key={name}
-              className="bg-muted/50 relative mt-8 flex flex-col justify-center items-center"
+              className='bg-muted/50 relative mt-8 flex flex-col justify-center items-center'
             >
-              <CardHeader className="mt-8 flex justify-center items-center pb-2">
+              <CardHeader className='mt-8 flex justify-center items-center pb-2'>
                 <img
                   src={imageUrl}
                   alt={`${name} ${position}`}
-                  className="absolute -top-12 rounded-full w-24 h-24 aspect-square object-cover"
+                  className='absolute -top-12 rounded-full w-24 h-24 aspect-square object-cover'
                 />
-                <CardTitle className="text-center">{name}</CardTitle>
-                <CardDescription className="text-primary">
+                <CardTitle className='text-center'>{name}</CardTitle>
+                <CardDescription className='text-primary'>
                   {position}
                 </CardDescription>
               </CardHeader>
 
-              <CardContent className="text-center pb-2">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+              <CardContent className='text-center pb-2'>
+                <p>{description}</p>
               </CardContent>
 
               <CardFooter>
                 {socialNetworks.map(({ name, url }: SociaNetworkslProps) => (
                   <div key={name}>
                     <a
-                      rel="noreferrer noopener"
+                      rel='noreferrer noopener'
                       href={url}
-                      target="_blank"
+                      target='_blank'
                       className={buttonVariants({
-                        variant: "ghost",
-                        size: "sm",
+                        variant: 'ghost',
+                        size: 'sm',
                       })}
                     >
-                      <span className="sr-only">{name} icon</span>
+                      <span className='sr-only'>{name} icon</span>
                       {socialIcon(name)}
                     </a>
                   </div>
